8bda4f4ce51a3cc54443a416e29a604c31e6585d
claw <claw@DESKTOP-4PG278Q>
2019-04-28 21:50:38 +0900

    엔티티 매니저로 저장

--- jpa-01/src/main/java/com/jpa/JpaRunner.java
+++ jpa-01/src/main/java/com/jpa/JpaRunner.java
@@ -3,6 +3,7 @@ package com.jpa;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -19,10 +20,10 @@ public class JpaRunner implements ApplicationRunner{
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
 		
+		//01. 엔티티 매니저로 저장
 		Account account = new Account();
 		account.setUsername("keesun");
 		account.setPassword("jap");
-		
 		entityManager.persist(account);
 	}
 	
