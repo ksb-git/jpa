d3b17a0fe2cb01ad0f1aa50226c6216e84314c15
claw <claw@DESKTOP-4PG278Q>
2019-04-28 21:56:30 +0900

    엔티티 매니저 저장

--- jpa-01/src/main/java/com/jpa/JpaRunner.java
+++ jpa-01/src/main/java/com/jpa/JpaRunner.java
@@ -3,7 +3,6 @@ package com.jpa;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
